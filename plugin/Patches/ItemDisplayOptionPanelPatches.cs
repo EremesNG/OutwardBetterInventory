8ebb2af9de4c8d6689f47045d6be4621961835ca
Faeryn <autistic.batman@gmail.com>
2022-06-05 15:48:26 +0200

    Patch refactor

--- plugin/Patches/ItemDisplayOptionPanelPatches.cs
+++ plugin/Patches/ItemDisplayOptionPanelPatches.cs
@@ -5,8 +5,9 @@ using HarmonyLib;
 using UnityEngine;
 
 namespace BetterInventory.Patches {
-
-	public static class ItemDisplayOptionPanelConsts {
+	
+	[HarmonyPatch]
+	public static class ItemDisplayOptionPanelPatches {
 		// Extra action IDs start at 4242 to avoid any potential conflicts
 		public static readonly Dictionary<int, ContextMenuAction> ExtraActions = new Dictionary<int, ContextMenuAction> {
 			{4242, new SalvageAction(4242)},
@@ -14,32 +15,23 @@ namespace BetterInventory.Patches {
 			{4244, new SendToHostStashAction(4244)},
 			{4245, new SendToOtherAction(4245)}
 		};
-	}
-	
-	[HarmonyPatch(typeof(ItemDisplayOptionPanel), nameof(ItemDisplayOptionPanel.GetActiveActions))]
-	public class EquipmentMenu_GetActiveActions {
-		[HarmonyPostfix]
-		static void Postfix(GameObject pointerPress, ref List<int> __result) {
-			__result.AddRange(from action in ItemDisplayOptionPanelConsts.ExtraActions.Values where action.IsActive(pointerPress) select action.ID);
+		
+		[HarmonyPatch(typeof(ItemDisplayOptionPanel), nameof(ItemDisplayOptionPanel.GetActiveActions)), HarmonyPostfix]
+		private static void EquipmentMenu_GetActiveActions_Postfix(GameObject pointerPress, ref List<int> __result) {
+			__result.AddRange(from action in ExtraActions.Values where action.IsActive(pointerPress) select action.ID);
 		}
-	}
-	
-	[HarmonyPatch(typeof(ItemDisplayOptionPanel), nameof(ItemDisplayOptionPanel.ActionHasBeenPressed))]
-	public class EquipmentMenu_ActionHasBeenPressed {
-		[HarmonyPostfix]
-		static void Prefix(ItemDisplayOptionPanel __instance, int _actionID) {
-			if (ItemDisplayOptionPanelConsts.ExtraActions.ContainsKey(_actionID)) {
-				ItemDisplayOptionPanelConsts.ExtraActions[_actionID].ExecuteAction(__instance);
+		
+		[HarmonyPatch(typeof(ItemDisplayOptionPanel), nameof(ItemDisplayOptionPanel.ActionHasBeenPressed)), HarmonyPrefix]
+		private static void EquipmentMenu_ActionHasBeenPressed_Prefix(ItemDisplayOptionPanel __instance, int _actionID) {
+			if (ExtraActions.ContainsKey(_actionID)) {
+				ExtraActions[_actionID].ExecuteAction(__instance);
 			}
 		}
-	}
-	
-	[HarmonyPatch(typeof(ItemDisplayOptionPanel), nameof(ItemDisplayOptionPanel.GetActionText))]
-	public class EquipmentMenu_GetActionText {
-		[HarmonyPostfix]
-		static bool Prefix(ItemDisplayOptionPanel __instance, int _actionID, ref string __result) {
-			if (ItemDisplayOptionPanelConsts.ExtraActions.ContainsKey(_actionID)) {
-				__result = ItemDisplayOptionPanelConsts.ExtraActions[_actionID].Text;
+		
+		[HarmonyPatch(typeof(ItemDisplayOptionPanel), nameof(ItemDisplayOptionPanel.GetActionText)), HarmonyPrefix]
+		private static bool EquipmentMenu_GetActionText_Prefix(ItemDisplayOptionPanel __instance, int _actionID, ref string __result) {
+			if (ExtraActions.ContainsKey(_actionID)) {
+				__result = ExtraActions[_actionID].Text;
 				return false;
 			}
 			return true;
